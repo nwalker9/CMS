6f2984597bc925956516853fe12fc212a4865c8a
Nicolas Walker <nicowalker29@gmail.com>
2026-06-01 09:56:28 -0400

    Added a main menu closes #3

--- CLI.CMS/CLI.CMS/Program.cs
+++ CLI.CMS/CLI.CMS/Program.cs
@@ -6,7 +6,38 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to the Content Management System");
+
+            Console.WriteLine("Choose a site to manage: ");
+
+            List<string> sites = new List<string>
+            {
+                "Site 1",
+                "Site 2",
+                "Site 3"
+            };
+            
+            int count = 0;
+            sites.ForEach(s => Console.WriteLine($"{++count}. {s}"));
+
+
+            var selection = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(selection))
+            {
+                Console.WriteLine(selection);
+
+
+                var match = sites.FirstOrDefault(s => s.Equals(selection, StringComparison.InvariantCultureIgnoreCase));
+
+                if (match != null)
+                {
+                    Console.WriteLine($"MATCHED: {match}");
+                } else
+                {
+                    Console.WriteLine("No match found");
+                }
+            }
         }
     }
 }
